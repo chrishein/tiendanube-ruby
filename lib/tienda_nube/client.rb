f79e27e27333d04075dc2d7993911edd28c20a79
Christian Hein <chrishein@gmail.com>
2019-01-11 15:52:53 -0300

    Allow setting User Agent to identify App when usign Client

--- lib/tienda_nube/client.rb
+++ lib/tienda_nube/client.rb
@@ -1,39 +1,40 @@
 module TiendaNube
     class Api
         class Client
-            def initialize(store_id, access_token)
+            def initialize(store_id, access_token, user_agent = nil)
                 @store_id = store_id
                 @access_token = access_token
                 @api_url = TiendaNube::API_URL
                 @api_version = TiendaNube::API_VERSION
+                @user_agent = user_agent
             end
 
             def store
-                @store ||= Store.new(@store_id, @access_token)
+                @store ||= Store.new(@store_id, @access_token, @user_agent)
             end
 
             def product
-                @product ||= Product.new(@store_id, @access_token)
+                @product ||= Product.new(@store_id, @access_token, @user_agent)
             end
 
             def category
-                @category ||= Category.new(@store_id, @access_token)
+                @category ||= Category.new(@store_id, @access_token, @user_agent)
             end
 
             def customer
-                @customer ||= Customer.new(@store_id, @access_token)
+                @customer ||= Customer.new(@store_id, @access_token, @user_agent)
             end
 
             def order
-                @order ||= Order.new(@store_id, @access_token)
+                @order ||= Order.new(@store_id, @access_token, @user_agent)
             end
 
             def product_image
-                @product_image ||= ProductImage.new(@store_id, @access_token)
+                @product_image ||= ProductImage.new(@store_id, @access_token, @user_agent)
             end
 
             def product_variamt
-                @product_variant ||= ProductVariant.new(@store_id, @access_token)
+                @product_variant ||= ProductVariant.new(@store_id, @access_token, @user_agent)
             end
         end
     end
